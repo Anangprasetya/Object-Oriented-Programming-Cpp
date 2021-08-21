1dde05a6a4657b8a09555e715b735456c1e30fcc
Anang <tyoandini@gmail.com>
2021-08-21 08:52:18 +0700

    materi polymorphism part 1 - basic

---
+++ 4-Polymorphism/part-1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Kendaraan {
+	protected :
+		int kecepatan, berat;
+		string medium;
+
+	public :
+		virtual void info() = 0;
+
+};
+
+
+class Darat : public Kendaraan {
+	public :
+		Darat(int k, int b, string m){
+			this->kecepatan = k;
+			this->berat = b;
+			this->medium = m;
+		}
+
+		void info(){
+			cout << "         Class Darat\n";
+			cout << "         Kecepatan : " << this->kecepatan << endl;
+			cout << "         Berat     : " << this->berat << endl;
+			cout << "         Medium    : " << this->medium << endl;
+		}
+};
+
+class Terbang : public Kendaraan {
+	public :
+		Terbang(int k, int b, string m){
+			this->kecepatan = k;
+			this->berat = b;
+			this->medium = m;
+		}
+
+		void info(){
+			cout << "         Class Terbang\n";
+			cout << "         Kecepatan : " << this->kecepatan << endl;
+			cout << "         Berat     : " << this->berat << endl;
+			cout << "         Medium    : " << this->medium << endl;
+		}
+};
+
+
+
+int main(){
+	Kendaraan * trans;
+	Darat mobil(90, 40, "Tanah");
+	Terbang jet(300, 120, "Udara");
+
+	trans = &mobil;
+	trans->info();
+
+	cout << endl;
+
+	trans = & jet;
+	trans->info();
+
+	return 0;
+}
+
+
+/*
+      * Compiler    :  G++ 9.3.0
+      * Materi      :  Polymorphism (Basic)
+      * OS          :  Linux Ubuntu
+      * Text Editor :  Sublime Text
+      * Sumber Ilmu :  https://www.tutorialspoint.com/index.htm
+      * Pembuat     :  Anang Nur Prasetya
+      * Tahun       :  2021
+*/
