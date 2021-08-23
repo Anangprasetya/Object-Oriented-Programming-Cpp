956ef23612501c8102001e69feb8b32762cd4fc5
Anang <tyoandini@gmail.com>
2021-08-23 20:35:45 +0700

    materi abstraction part 1 - basic

---
+++ 5-Abstraction/part-1.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Struktur {
+	public :
+		void prima(int a, int b){
+			this->n = b;
+			int i;
+			int hasil[n];
+			bil=a; 
+		    while(bil<=n){
+		    	cek=0;
+	            for(int j=2;j<=bil;j++){
+                  if(bil%j==0){
+                        cek++; 
+                  }
+	            }
+	            if(cek==1){
+	                 hasil[i]=bil; //simpan prima di array
+	                 i++;          //penambahan index array
+	            }
+	            bil++;
+		    }
+		    this->cetak(hasil, i);
+
+		}
+
+
+		void fibonannci(int a){
+			int fibo_n, fibo_n2=1, fibo_n1=0;
+			int hasil[a];
+			int x = 0;
+
+			for (int i = 0; i < a; i++)
+			{
+				fibo_n = fibo_n1 + fibo_n2;
+				hasil[x] = fibo_n1;
+				x++;
+
+
+				fibo_n1 = fibo_n2;
+				fibo_n2 = fibo_n;
+			}
+
+		    this->cetak(hasil, x);
+
+
+		}
+
+
+	private :
+		int bil;
+		int n;
+		int cek;
+
+		void cetak(int x[], int y){
+			for(int j=0;j<y;j++){ //mencetak semua bilangan prima
+            cout<<x[j];
+              if(j < y-1){ 
+                   cout<<", ";
+              }else{
+                   cout<<".\n";
+              }
+	      }
+		}
+};
+
+
+
+int main(){
+	Struktur obj;
+
+	obj.prima(3, 15);
+	obj.fibonannci(6);
+
+	return 0;
+}
+
+
+/*
+      * Compiler    :  G++ 9.3.0
+      * Materi      :  Abstraction (Basic)
+      * OS          :  Linux Ubuntu
+      * Text Editor :  Sublime Text
+      * Sumber Ilmu :  https://www.tutorialspoint.com/index.htm
+      * Pembuat     :  Anang Nur Prasetya
+      * Tahun       :  2021
+*/
